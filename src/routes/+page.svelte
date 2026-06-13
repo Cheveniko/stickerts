efe0c44807cb3fee1acad4df91a8d3e26a0e9803
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2026-06-13 11:19:54 -0500

    xd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import * as m from "$lib/paraglide/messages";
   import Faq from "$lib/components/faq.svelte";
   import ListingsGrid from "$lib/components/listings-grid.svelte";
   import SalesGrid from "$lib/components/sales-grid.svelte";
 </script>
 
 <main class="container space-y-6 pt-20 pb-16 md:pt-30">
-  <!-- <h2 class="text-xl font-semibold">{m.home_heading()}</h2> -->
   <ListingsGrid />
   <SalesGrid />
   <Faq />
