d9706702a1c84dcfe25fec1f7ce7fc05294c434e
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2026-06-18 12:07:50 -0500

    gerardito 🫶🏻

--- src/convex/cities.ts
+++ src/convex/cities.ts
@@ -1,6 +1,7 @@
 import { v } from "convex/values";
-import { query, type QueryCtx } from "./_generated/server";
+import { internalMutation, query, type QueryCtx } from "./_generated/server";
 import type { Doc } from "./_generated/dataModel";
+import { locationSeed, toLocationSlug } from "../lib/location-data";
 
 export type City = Doc<"cities">;
 
@@ -25,5 +26,43 @@ export const listCitiesByCountryCode = query({
         q.eq("countryCode", args.countryCode),
       )
       .take(500);
+  },
+});
+
+export const syncLocationSeed = internalMutation({
+  args: {},
+  handler: async (ctx) => {
+    let created = 0;
+    let updated = 0;
+
+    for (const country of locationSeed) {
+      for (const cityName of country.cities) {
+        const slug = toLocationSlug(cityName);
+        const existingCity = await ctx.db
+          .query("cities")
+          .withIndex("by_slug", (q) => q.eq("slug", slug))
+          .unique();
+
+        const cityFields = {
+          countryCode: country.code,
+          countryName: country.name,
+          currency: country.currency,
+          currencySymbol: country.currencySymbol,
+          flagEmoji: country.flagEmoji,
+          name: cityName,
+          slug,
+        };
+
+        if (existingCity) {
+          await ctx.db.patch(existingCity._id, cityFields);
+          updated += 1;
+        } else {
+          await ctx.db.insert("cities", cityFields);
+          created += 1;
+        }
+      }
+    }
+
+    return { created, updated, total: created + updated };
   },
 });
